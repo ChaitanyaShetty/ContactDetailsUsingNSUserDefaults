75cab588619471d4a3037c4f711ebfca4d59d026
test <test@tests-Mac.local>
2017-01-06 13:10:09 -0200

    Initial Commit

---
+++ contactDetails/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  contactDetails
+//
+//  Created by test on 1/6/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
